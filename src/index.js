7af82671c507d855b3afcd079e17640ab1bc9282
Joaquin <joaquinovejero.ps@gmail.com>
2026-08-07 17:31:29 +0000

    Formato menor en index.js

--- src/index.js
+++ src/index.js
@@ -13,6 +13,7 @@ app.get('/api/health', (req, res) => {
 });
 
 const PORT = process.env.PORT || 3000;
+
 app.use('/api/auth', authRoutes);
 app.listen(PORT, () => {
   console.log(`Servidor corriendo en el puerto ${PORT}`);
