0976456af7b1c61a1ac770fc804addf907f2fd63
Joaquin <joaquinovejero.ps@gmail.com>
2026-08-14 16:06:30 +0000

    Zarpado unificado: eliminar boolean, zarpadoEn como fecha real de zarpe

---
+++ prisma/migrations/20260814160555_zarpado_solo_fecha/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `zarpado` on the `bookings` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "bookings" DROP COLUMN "zarpado";
